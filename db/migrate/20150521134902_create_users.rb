244ce2bb148f1cbfb16daac154547d37c4be0020
spenserhuang <spenserhuang515@gmail.com>
2015-05-21 19:15:10 -0700

    Fixed all bugs for MVP

--- db/migrate/20150521134902_create_users.rb
+++ db/migrate/20150521134902_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name,         null: false
       t.string :last_name,          null: false
       t.string :username,           null: false
-      t.string :password_digest,    null: false
+      t.string :password_hash,      null: false
       t.string :email,              null: false
       t.string :phone_number,       null: false
 
--- db/migrate/20150521170607_update_column_name_users.rb
+++
@@ -1,5 +0,0 @@
-class UpdateColumnNameUsers < ActiveRecord::Migration
-  def change
-    rename_column :users, :password_digest, :password_hash
-  end
-end
