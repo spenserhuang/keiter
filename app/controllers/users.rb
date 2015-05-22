b5c8f6c8d8f5cee58d896f3d211e1ffc27237a5c
spenserhuang <spenserhuang515@gmail.com>
2015-05-22 15:17:09 -0700

    Completed fucking relationships

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -5,8 +5,11 @@ end
 
 
 get '/users/:username' do
-  @user = User.find_by(username: params[:username])
-  @keit = Keit.where(user_id: @user.id)
+  @user1 = User.find_by(username: params[:username])
+  "*" * 15
+  p @user1
+  "*" * 15
+  @keit = Keit.where(user_id: @user1.id)
   erb :'/users/index'
 end
 
@@ -25,4 +28,9 @@ put '/users/profile' do
   @editprofile = User.find_by(id: session[:user_id])
   @editprofile.update_attributes(username: params[:username], password_hash: params[:password_hash], first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
   redirect '/users/profile'
+end
+
+get '/users/profile/relationships' do
+  @user = current_user
+  erb :'/users/relationships'
 end
